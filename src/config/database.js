772a2f8e4e0a8a14703147161f1f42337f4b1f9e
francisco1code <francisco170142329@gmail.com>
2020-09-17 00:19:05 -0300

    feature(#35): configuring and creating the first migrations and organizing folders

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+    difine: {
+        timetamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+}
