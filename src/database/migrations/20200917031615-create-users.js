54e086b9f0a55dc4b168f8a3d0d91ff0181c91b9
francisco1code <francisco170142329@gmail.com>
2020-09-17 01:02:16 -0300

    feature(#35): creating user table in postgres

--- src/database/migrations/20200917031615-create-users.js
+++ src/database/migrations/20200917031615-create-users.js
@@ -2,20 +2,57 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    
+    return queryInterface.createTable('users', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIcrement: true,
+        primaryKey: true
+        
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+
+      },
+      cpf: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+
+      },
+      telefone: {
+        type: Sequelize.STRING,
+        allowNull: false
+
+      },
+      senha_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+
+      },
+      created_at: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+
+    
+    });
+     
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: queryInterface => {
+    queryInterface.dropTable('users');
   }
 };
