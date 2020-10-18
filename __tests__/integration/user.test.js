138ec26e1eaf4cb2a94e9cf51d3cbe94d3e57d54
francisco1code <francisco170142329@gmail.com>
2020-10-17 23:12:12 -0300

    feat(#104): generation of random data for testing with factory and faker libraries

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../../src/app'
 import bcrypt from 'bcryptjs'
 import truncate from '../util/truncate'
-import User from '../../src/app/models/User'
+import factory from '../factores'
 
 describe('User', () => {
 
@@ -11,14 +11,10 @@ describe('User', () => {
     })
 
     it('should encrypt user password when new user create', async() => {
-        const user = await User.create({
-            id: "1",
-            name: "admin",
-            email: "admin@gmail.com", 
-            telephone: "3254124",
-            password: "123456"
+        const user = await factory.create('User',{
+            password: "123456",
         })
-        
+         
         const compareHash = await bcrypt.compare('123456', user.password_hash)
 
         expect(compareHash).toBe(true);
@@ -26,40 +22,25 @@ describe('User', () => {
     })
 
     it('should be able to register', async() => {
+        const user = await factory.attrs('User')
+
         const response = await request(app)
             .post('/users')
-            .send({
-                id: "1",
-	            name: "admin",
-	            email: "admin@gmail.com",
-	            telephone: "3254124",
-	            password: "123456"
-            })
+            .send(user)
 
         expect(response.body).toHaveProperty('id')
     })
     
     it('should not be able to resgister with duplicated email', async () => {
+        const user = await factory.attrs('User')
 
         await request(app)
         .post('/users')
-        .send({
-            id: "1",
-	        name: "admin",
-	        email: "admin@gmail.com",
-	        telephone: "3254124",
-	        password: "123456"
-        });
+        .send(user);
 
         const response = await request(app)
         .post('/users')
-        .send({
-            id: "1",
-	        name: "admin",
-	        email: "admin@gmail.com",
-	        telephone: "3254124",
-	        password: "123456"
-        })
+        .send(user)
 
         expect(response.status).toBe(400);
 
