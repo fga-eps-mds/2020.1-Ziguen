b77187c1a1a8e72fa71c9dc1b6fd41c8195ec2d3
francisco1code <francisco170142329@gmail.com>
2020-10-17 21:52:11 -0300

    feat(#104): should not be able to resgister with duplicated email

---
+++ __tests__/integration/user.test.js
@@ -0,0 +1,46 @@
+import request from 'supertest'
+import app from '../../src/app'
+
+describe('User', () => {
+    it('shold be able to register', async() => {
+        const response = await request(app)
+            .post('/users')
+            .send({
+                id: "1",
+	            name: "admin",
+	            email: "admin@gmail.com",
+	            telephone: "3254124",
+	            password: "123456"
+            })
+
+        expect(response.body).toHaveProperty('id')
+    })
+    
+    it('shold not be able to resgister with duplicated email', async () => {
+
+        await request(app)
+        .post('/users')
+        .send({
+            id: "1",
+	        name: "admin",
+	        email: "admin@gmail.com",
+	        telephone: "3254124",
+	        password: "123456"
+        });
+
+        const response = await request(app)
+        .post('/users')
+        .send({
+            id: "1",
+	        name: "admin",
+	        email: "admin@gmail.com",
+	        telephone: "3254124",
+	        password: "123456"
+        })
+
+        expect(response.status).toBe(400);
+
+    })
+
+
+})
