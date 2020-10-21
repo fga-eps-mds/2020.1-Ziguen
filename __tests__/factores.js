fa909a7684e10093956376d81a72bfe9987bb395
francisco1code <francisco170142329@gmail.com>
2020-10-21 18:44:18 -0300

    solving the CI test break

--- __tests__/factores.js
+++ __tests__/factores.js
@@ -4,7 +4,7 @@ import { factory} from 'factory-girl'
 import User from '../src/app/models/User'
 
  factory.define('User', User, {
-    id: faker.random.number, 
+    cpf: faker.random.number,
     name: faker.name.findName,
     email: faker.internet.email, 
     telephone: faker.random.number,
