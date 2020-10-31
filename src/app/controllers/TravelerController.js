353cc45ebfb9d51d750f00b8e887b211511c90e6
liversonp <liverson.p@gmail.com>
2020-10-31 20:10:17 -0300

    fix(#121): Added try catch to traveler
    Co-authored-by: francisco1code <francisco170142329@gmail.com>

--- src/app/controllers/TravelerController.js
+++ src/app/controllers/TravelerController.js
@@ -1,112 +1,91 @@
-import * as  Yup from 'yup';
 import Traveler from '../models/Traveler';
 
 class TravelerController{
-  async list(req, res){
-    const {name} = req.query;
-    const travelers = await Traveler.findAll({name});
-    return res.json(travelers);
+  async index(req, res){
+    try{
+      const {name} = req.query;
+      const travelers = await Traveler.findAll({name});
+      return res.json(travelers);
+    }catch(err){
+      return res.status(500).json({error: 'Falha ao listar'})
+    }
   }
   
   async store(req, res){
-    const schema = Yup.object().shape({
-      name: Yup.string().required(),
-      cpf: Yup.string().required().min(11),
-      email: Yup.string().required(),
-      password: Yup.string().required().min(6),
-    })
+    try{
+      const travelerExists = await Traveler.findOne({ 
+        where: { cpf: req.body.cpf }, 
+      });
     
-    if(!(await schema.isValid(req.body))){
+      if (travelerExists) {
+        return res.status(400).json({ error: 'Usuario já Cadastrado' });
+      }
+    
+      const {  name, cpf, email } = await Traveler.create(req.body);
+    
+      return res.json({
+        name,
+        cpf,
+        email,
+      }); 
+    }catch(err){
       return res.status(400).json({ error: 'Falha na validação das informações.' });
     }
-    
-    const travelerExists = await Traveler.findOne({ 
-      where: { cpf: req.body.cpf }, 
-
-    });
-    
-    if (travelerExists) {
-      return res.status(400).json({ error: 'Usuario já Cadastrado' });
-    }
-    
-    const {  name, cpf, email } = await Traveler.create(req.body);
-    
-    return res.json({
-  
-      name,
-      cpf,
-      email,
-    }); 
   }
 
   async update(req,res){
-    const { email, oldpassword } = req.body;
+    try{
+      const { id } = req.body;
 
-    const traveler = await Traveler.findByPk(req.userId);
-
-    if(email != traveler.email){
-      const travelerExists = await Traveler.findOne({ 
-        where: { email } 
-      });
+      const traveler = await Traveler.findByPk(req.userId);
   
-      if (travelerExists) {
-        return res.status(400).json({ error: 'Usuario já Cadastrado.' });
+      if (!travelerExists) {
+        return res.status(400).json({ error: 'Usuario não existe.' });
       }
+
+      const {name,cpf,email,telephone,password} = await adminExists.update(req.body)
+
+      return res.json({
+        name,
+        cpf,
+        email,
+        telephone,
+        password
+      });
+    }catch(err){
+      return res.status(400).json({ error: 'Usuario não existe.' });
     }
-
-    if(oldpassword && !(await traveler.checkPassword(oldpassword))){
-      return res.status(400).json({ error: 'Senha incorreta.' });
-    }
-
-    const  {id , name } = await traveler.update(req.body);
-
-
-    return res.json({
-      id,
-      name,
-      email,
-    });
   }
 
   async destroy(req, res){
-    const schema = Yup.object().shape({
-      id: Yup.number().required().positive()
-    });
+    try{
+      const {id} = req.body;
+      const traveler = await Traveler.findByPk(id);
 
-    if(!(await schema.isValid(req.body))){
-      return res.status(400).json({error:"Informações incoerentes"});
+      if(!traveler){
+        return res.json({error:"Usuário inexistente"});
+      }
+      await traveler.destroy();
+      return res.status(200).json({message:"excluído com sucesso"});
+    
+    }catch(err){
+      return res.status(400).json({ error: 'falha na exclusão.' });
     }
-
-    const {id} = req.body;
-
-    const traveler = await Traveler.findByPk(id);
-
-    if(!traveler){
-      return res.json({error:"Usuário inexistente"});
-    }
-
-    await traveler.destroy();
-    return res.status(200).json({message:"excluído com sucesso"});
-
   }
 
   async descript(req, res){
-    const schema = Yup.object().shape({
-      id: Yup.number().required().positive()
-    });
-    if(!(await schema.isValid(req.body))){
-      return res.status(400).json({error:"Informações incoerentes"});
-    }
+    try{
+      const {id} = req.body;
+      const traveler = await Traveler.findByPk(id);
 
-    const {id} = req.body;
-
-    const traveler = await Traveler.findByPk(id);
-
-    if(!traveler){
-      return res.json({error:"Usuário inexistente"});
-    }
+      if(!traveler){
+        return res.json({error:"Usuário inexistente"});
+      }
     
-    return res.json(traveler);
+      return res.json(traveler);
+    }catch(err){
+      return res.status(400).json({ error: 'falha de informações.' });
+    }
   }
 }
 
